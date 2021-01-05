735a5f60ec5d2ae41b1532f532910e09dc584899
zhouxv <1598284508@qq.com>
2021-01-05 19:20:16 +0800

    util方法修改 1. ConvertList类中修改static方法stringToDoubleList(),增加sout来帮助开发者观察函数内部.stringToDoubleList()、stringToDoubleArray()增加对传入字符串内容是否为数字的检验. 2. 由于数据库中sagLevel表中sResult、aResult、gResult的内容格式变化(由"1"->"0.98,0.83,0.81,0.79,1") ,CountSagLevel类中totalCount()和countFinalLevel()都要作相应修改. 3. CountIndexLevel类中indexLevel()中,因为有updateTestResult的需求,数组大小的检验变为8或9;循环中不再使用'+',而是使用StringBuffer.

--- src/test/java/com/issue1/demo/testMain.java
+++ src/test/java/com/issue1/demo/testMain.java
@@ -1,13 +1,11 @@
 package com.issue1.demo;
 
-import com.issue1.demo.util.*;
-
-import static com.issue1.demo.util.ConvertList.stringToDoubleList;
+import static com.issue1.demo.util.CountIndexLevel.indexLevel;
+import static com.issue1.demo.util.CountSagLevel.countFinalLevel;
 
 public class testMain {
     public static void main(String[] args) {
-//        String[] strings={"边界防护","访问控制","身份鉴别","密码支持","入侵防范","监控与检测","恶意代码防范","数据保密性","数据完整性","数据和应用可用性","数据销毁","数据备份恢复","个人数据恢复","安全审计与验证","机构配置管理","剩余信息保护","安全策略","事件预警和处置","管理制度","安全日志","流程管理","服务协议","电子合同"};
-//        System.out.println(strings.length);
+        System.out.println(indexLevel("2,3,2,2,3,4,1,3,9"));
     }
 
 }
